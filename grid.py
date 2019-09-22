6977fb5134e56884de4b19006a0929ae5109cc92
HengruiZhan <48325976+HengruiZhan@users.noreply.github.com>
2019-09-22 16:36:56 -0400

    Update grid.py

--- grid.py
+++ grid.py
@@ -47,19 +47,8 @@ class Grid():
 
         self.r2d, self.z2d = np.meshgrid(self.r, self.z, indexing='ij')
 
-        self.vol = np.pi*2.0*self.r2d*self.dr*self.dz
-
-    """
-    def set_bin(self, inner, inner_max, s, t):
-        # inner is used to dtermine the bin size
-        # in terms of atomic radial measure
-        # s and t are used to determined the outer radial m_zone
-        # see section 8.10.2.2 of the Flash User's Guide
-        # Multipole Poisson solver (improved version)
-        self.bin = []
-        self.bin[0] = inner*dr
-        while
-        """
+        self.vol = np.pi*((self.r2d+self.dr/2)**2 -
+                          (self.r2d-self.dr/2)**2)*self.dz
 
     def scratch_array(self):
         return np.zeros((self.nr, self.nz), dtype=np.float64)
