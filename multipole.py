fe001f342618b54b1f9b7f783fbbd4254f9eb8a2
HengruiZhan <48325976+HengruiZhan@users.noreply.github.com>
2019-09-23 16:42:25 -0400

    Update multipole.py

--- multipole.py
+++ multipole.py
@@ -172,3 +172,131 @@ class Multipole():
                 phi[i, j] = self.phi_point(self.g.r[i], self.g.z[j])
         return phi
         """
+    
+    
+    #brute force version of multipole expansion
+    class Multipole():
+    # def __init__(self, grid, n_moments, dr, center=(0.0, 0.0)):
+    def __init__(self, grid, n_moments, center=(0.0, 0.0)):
+
+        self.g = grid
+        self.n_moments = n_moments
+        self.center = center
+
+        self.radius = np.sqrt((self.g.r2d - self.center[0])**2 +
+                              (self.g.z2d - self.center[1])**2)
+
+        self.m_r = []
+        self.m_i = []
+        for l in range(self.n_moments+1):
+            self.m_r.append(self.g.complex_array())
+            self.m_i.append(self.g.complex_array())
+
+    @jit
+    def compute_expansion(self, rho):
+        # rho is density that lives on a grid self.g
+
+        # loop over cells
+        for i in range(self.g.nr):
+            for j in range(self.g.nz):
+                radius = np.sqrt((self.g.r[i] - self.center[0])**2 +
+                                 (self.g.z[j] - self.center[1])**2)
+                # tan(theta) = r/z
+                theta = np.arctan2(self.g.r[i], self.g.z[j])
+                m_zone = rho[i, j] * self.g.vol[i, j]
+                # loop over the multipole moments, l (m = 0 here)
+                for l in range(self.n_moments+1):
+                    # compute Y_l^m (note: we use theta as the polar
+                    # angle, scipy is opposite)
+                    Y_lm = sph_harm(0, l, 0.0, theta)
+
+                    R_lm = np.sqrt(4*np.pi/(2*l + 1)) * radius**l * Y_lm
+                    I_lm = np.sqrt(4*np.pi/(2*l + 1)) * Y_lm / radius**(l+1)
+
+                    # assign to the the  inner or outer moment functions
+                    self.m_r[l][i, j] = R_lm * m_zone
+                    self.m_i[l][i, j] = I_lm * m_zone
+
+    @jit
+    def compute_phi_face(self, dr, dz):
+        # return Phi(r), using Eq. 20
+
+        r_face = self.g.r2d + dr
+        z_face = self.g.z2d + dz
+
+        radius = np.sqrt((r_face - self.center[0])**2 +
+                         (z_face - self.center[1])**2)
+
+        # tan(theta) = r/z
+        theta = np.arctan2(r_face, z_face)
+
+        phi_zone = self.g.complex_array()
+        for l in range(self.n_moments+1):
+
+            Y_lm = sph_harm(0, l, 0.0, theta)
+            R_lm = np.sqrt(4*np.pi/(2*l + 1)) * radius**l * Y_lm
+            I_lm = np.nan_to_num(np.sqrt(4*np.pi/(2*l + 1)) *
+                                 Y_lm / radius**(l+1))
+
+            # loop over each cell to assign the face potential
+
+            for i in range(self.g.nr):
+                for j in range(self.g.nz):
+                    inmask = self.radius <= radius[i, j]
+                    outmask = self.radius > radius[i, j]
+
+                    # equation 20
+                    # buggy
+
+                    phi_zone[i, j] += sc.G * (np.sum(self.m_r[inmask]) *
+                                              np.conj(I_lm[i, j]) +
+                                              np.conj(np.sum(
+                                                  self.m_i[outmask])) *
+                                              R_lm[i, j])
+
+        return -np.real(phi_zone)
+
+    @jit
+    def phi(self):
+        # calculate the potential of a specific point
+        dr = self.g.dr/2
+        dz = self.g.dz/2
+
+        phi = self.g.scratch_array()
+
+        # phi_0 = self.compute_phi(r, z, 0, 0)
+
+        phi_m_r = self.compute_phi_face(-dr, 0)
+        phi_m_z = self.compute_phi_face(0, -dz)
+        phi_p_r = self.compute_phi_face(dr, 0)
+        phi_p_z = self.compute_phi_face(0, dz)
+
+        # phi = (phi_m_r+phi_m_z+phi_p_r+phi_p_z)/4
+
+        # phi = 1/total_area(sum(phi(x_face)*area))
+        # compute the areas in different direction
+
+        area_m_r = self.g.scratch_array()
+        area_m_z = self.g.scratch_array()
+        area_p_r = self.g.scratch_array()
+        area_p_z = self.g.scratch_array()
+        total_area = self.g.scratch_array()
+
+        for i in range(self.g.nr):
+            for j in range(self.g.nz):
+                area_m_r[i, j] = 2*np.pi*(self.g.r2d[i, j]-dr)*dz
+                area_m_z[i, j] = np.pi*((self.g.r2d[i, j]+dr)**2 -
+                                        (self.g.r2d[i, j]-dr)**2)
+                area_p_r[i, j] = 2*np.pi*(self.g.r2d[i, j]+dr)*dz
+                area_p_z[i, j] = np.pi*((self.g.r2d[i, j]+dr)**2 -
+                                        (self.g.r2d[i, j]-dr)**2)
+                total_area[i, j] = area_m_r[i, j]+area_m_z[i, j] +\
+                    area_p_r[i, j]+area_p_z[i, j]
+
+        phi_m_r_area = phi_m_r*area_m_r
+        phi_m_z_area = phi_m_z*area_m_z
+        phi_p_r_area = phi_p_r*area_p_r
+        phi_p_z_area = phi_p_z*area_p_z
+        phi = (phi_m_r_area+phi_m_z_area+phi_p_r_area+phi_p_z_area)/total_area
+
+        return phi
