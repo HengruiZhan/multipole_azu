e10abd4d7d3056200852c48b53ec4d7d8b509885
HengruiZhan <48325976+HengruiZhan@users.noreply.github.com>
2019-09-24 16:23:04 -0400

    Update L2_difference.py

--- L2_difference.py
+++ L2_difference.py
@@ -7,3 +7,11 @@ def L2_diff(analytical_phi, approximate_phi, gcenter):
     pot_diff_norm = np.sqrt(np.sum(np.square(phi_difference)/N))
     # pot_diff_norm = np.sqrt(np.sum(np.square(phi_difference)))
     return pot_diff_norm
+
+
+# finite volume L2 norm error
+def L2_diff(analytical_phi, approximate_phi, grid):
+    total_vol = np.sum(grid.vol)
+    phi_difference = analytical_phi - approximate_phi
+    L2_norm = np.sqrt(np.sum(phi_difference**2)/total_vol)
+    return L2_norm
