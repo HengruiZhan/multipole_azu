3f348d40e4419bb31eba79dabd916fb3a15ed7f0
HengruiZhan <48325976+HengruiZhan@users.noreply.github.com>
2019-09-25 08:02:40 -0400

    Update L2_difference.py

--- L2_difference.py
+++ L2_difference.py
@@ -4,9 +4,8 @@ import numpy as np
 def L2_diff(analytical_phi, approximate_phi, gcenter):
     phi_difference = analytical_phi - approximate_phi
     N = gcenter.size
-    pot_diff_norm = np.sqrt(np.sum(np.square(phi_difference)/N))
-    # pot_diff_norm = np.sqrt(np.sum(np.square(phi_difference)))
-    return pot_diff_norm
+    L2_norm = np.sqrt(np.sum(np.square(phi_difference)/N))
+    return L2_norm
     """
 
 
